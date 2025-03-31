4c9aeb103dfb0d348232842b1836155aa17446d0
minhnbnt <minhnbnt@gmail.com>
2025-03-31 10:15:36 +0700

    style: format code

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,52 @@
-import React from 'react'
-import Input from '@/components/Input'
-import Link from 'next/link'
+import React from 'react';
+import Input from '@/components/Input';
+import Link from 'next/link';
 
-type loginProps = {}
+type loginProps = {};
 
-export default function Login({ }: loginProps) {
-    return (
-        <div className="flex justify-center items-center fixed top-0 left-0 right-0 bottom-0 bg-black/30">
-            <div className="w-[55%] h-[60%] border-[1px] border-solid border-white rounded-[10px] flex justify-center items-center p-[10px]">
-                <div className="w-[45%] flex flex-col items-center">
-                    <img src="./logo.jpeg" alt="" className="rounded-[50%] w-[70px] h-[70px]" />
-                    <h1 className="w-full text-center">Log in connect with your friend!</h1>
-                </div>
-                <form className="w-[55%] h-full border-[1px] border-solid border-white rounded-[10px] p-[10px] shadow-[2px_2px_2px_grey]">
-                    <Input id="username" type="text" label="username" refElement={undefined} validation='' error='' />
-                    <Input id="password" type="password" label="password" refElement={undefined} validation='' error='' />
-                    <div className="flex justify-between items-center px-[10px]">
-                        <p>Don't have an account?</p>
-                        <Link href="/signup">Sign up</Link>
-                    </div>
-                    <button className="w-full cursor-pointer h-[30px] bg-blue-600 rounded-[8px] mt-[10px]" type="submit">Log in</button>
-                </form>
-            </div>
-        </div>
-    )
+export default function Login({}: loginProps) {
+	return (
+		<div className="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center bg-black/30">
+			<div className="flex h-[60%] w-[55%] items-center justify-center rounded-[10px] border-[1px] border-solid border-white p-[10px]">
+				<div className="flex w-[45%] flex-col items-center">
+					<img
+						src="./logo.jpeg"
+						alt=""
+						className="h-[70px] w-[70px] rounded-[50%]"
+					/>
+					<h1 className="w-full text-center">
+						Log in connect with your friend!
+					</h1>
+				</div>
+				<form className="h-full w-[55%] rounded-[10px] border-[1px] border-solid border-white p-[10px] shadow-[2px_2px_2px_grey]">
+					<Input
+						id="username"
+						type="text"
+						label="username"
+						refElement={undefined}
+						validation=""
+						error=""
+					/>
+					<Input
+						id="password"
+						type="password"
+						label="password"
+						refElement={undefined}
+						validation=""
+						error=""
+					/>
+					<div className="flex items-center justify-between px-[10px]">
+						<p>Don't have an account?</p>
+						<Link href="/signup">Sign up</Link>
+					</div>
+					<button
+						className="mt-[10px] h-[30px] w-full cursor-pointer rounded-[8px] bg-blue-600"
+						type="submit"
+					>
+						Log in
+					</button>
+				</form>
+			</div>
+		</div>
+	);
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,30 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-type HeaderProps = {
-
-}
+type HeaderProps = {};
 
 function Header() {
-    return (
-        <header className="h-[50px] fixed top-[5px] right-[5px] left-[5px] bg-black/50 flex items-center z-10 rounded-[10px] p-[5px] px-[10px]">
-            <img
-                src="./logo.jpeg"
-                alt=""
-                className="w-auto h-full rounded-[50%] ml-[5%] border-[1px] border-solid border-white"
-            />
-            <div className="mx-auto w-[50%] flex justify-evenly">
-                <Link href="/home">Home</Link>
-                <button>Search</button>
-                <button>Contact us</button>
-                <button>Report</button>
-            </div>
-            <div className="flex justify-center items-center gap-[10px]">
-                <Link href="/login">Log in</Link>
-                <div className='w-[1px] h-[25px] bg-white'></div>
-                <Link href="/signup">Sign up</Link>
-            </div>
-        </header>
-    )
+	return (
+		<header className="fixed top-[5px] right-[5px] left-[5px] z-10 flex h-[50px] items-center rounded-[10px] bg-black/50 p-[5px] px-[10px]">
+			<img
+				src="./logo.jpeg"
+				alt=""
+				className="ml-[5%] h-full w-auto rounded-[50%] border-[1px] border-solid border-white"
+			/>
+			<div className="mx-auto flex w-[50%] justify-evenly">
+				<Link href="/home">Home</Link>
+				<button>Search</button>
+				<button>Contact us</button>
+				<button>Report</button>
+			</div>
+			<div className="flex items-center justify-center gap-[10px]">
+				<Link href="/login">Log in</Link>
+				<div className="h-[25px] w-[1px] bg-white"></div>
+				<Link href="/signup">Sign up</Link>
+			</div>
+		</header>
+	);
 }
 
-export default Header
+export default Header;
