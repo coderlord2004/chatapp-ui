0987757a021ed4d9b11df3c2dd9ceae189dda766
minhnbnt <minhnbnt@gmail.com>
2025-05-09 11:22:42 +0700

    feat: axios now will accept and send msgpack data

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,80 +1,107 @@
-'use client'
+'use client';
 
-import axios from 'axios'
+import axios, {
+	type AxiosRequestHeaders,
+	type AxiosResponseHeaders,
+} from 'axios';
+
+import { decode, encode } from '@msgpack/msgpack';
 
 const getAccessToken = () => {
-	if (typeof window !== 'undefined') {
-		return localStorage.getItem('accessToken')
+	if (typeof window === 'undefined') {
+		return null;
 	}
-	return null
-}
+	return localStorage.getItem('accessToken');
+};
 
 const getRefreshToken = () => {
-	if (typeof window !== 'undefined') {
-		return localStorage.getItem('refreshToken')
+	if (typeof window === 'undefined') {
+		return null;
 	}
-	return null
-}
+	return localStorage.getItem('refreshToken');
+};
 
 const logout = () => {
-	localStorage.removeItem('accessToken')
-	localStorage.removeItem('refreshToken')
-	window.location.href = '/login'
-}
+	localStorage.removeItem('accessToken');
+	localStorage.removeItem('refreshToken');
+	window.location.href = '/login';
+};
 
 const refreshAccessToken = async () => {
 	try {
-		const refreshToken = getRefreshToken()
-		if (!refreshToken) throw new Error('No refresh token')
+		const refreshToken = getRefreshToken();
+		if (!refreshToken) throw new Error('No refresh token');
 
 		const response = await axios.post(
 			`${process.env.NEXT_PUBLIC_WEBCHAT_BASE_URL}/users/token/refresh/`,
-			{ refresh: refreshToken }
-		)
+			{ refresh: refreshToken },
+		);
 
-		const newAccessToken = response.data.access
-		localStorage.setItem('accessToken', newAccessToken)
-		return newAccessToken
+		const newAccessToken = response.data.access;
+		localStorage.setItem('accessToken', newAccessToken);
+		return newAccessToken;
 	} catch (err) {
-		logout()
-		throw err
+		logout();
+		throw err;
 	}
+};
+
+const MSGPACK_CONTENT_TYPE = 'application/msgpack';
+
+function encodeRequest(data: unknown, headers: AxiosRequestHeaders) {
+	if (!(data instanceof FormData)) {
+		headers['Content-Type'] = MSGPACK_CONTENT_TYPE;
+		data = encode(data);
+	}
+
+	return data;
+}
+
+function decodeResponse(data: ArrayBuffer, headers: AxiosResponseHeaders) {
+	if (headers['content-type'] === MSGPACK_CONTENT_TYPE) {
+		return decode(data);
+	}
+
+	return data;
 }
 
 const request = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_WEBCHAT_BASE_URL,
+	headers: { Accept: MSGPACK_CONTENT_TYPE },
+	transformResponse: [decodeResponse],
+	transformRequest: [encodeRequest],
+	responseType: 'arraybuffer',
 	timeout: 15000,
-})
+});
 
 request.interceptors.request.use(
 	(config) => {
-		const token = getAccessToken()
+		const token = getAccessToken();
 		if (token) {
-			config.headers.Authorization = `Bearer ${token}`
+			config.headers.Authorization = `Bearer ${token}`;
 		}
-		return config
+		return config;
 	},
-	(error) => Promise.reject(error)
-)
+	(error) => Promise.reject(error),
+);
 
 request.interceptors.response.use(
 	(response) => response,
 	async (error) => {
-		const originalRequest = error.config
+		const originalRequest = error.config;
+		const accessTokenExpired =
+			error.response?.status === 401 && !originalRequest._retry;
 
-		if (error.response?.status === 401 && !originalRequest._retry) {
-			originalRequest._retry = true
-			try {
-				const newAccessToken = await refreshAccessToken()
-				originalRequest.headers.Authorization = `Bearer ${newAccessToken}`
-				return request(originalRequest)
-			} catch (refreshError) {
-				return Promise.reject(refreshError)
-			}
+		if (!accessTokenExpired) {
+			return Promise.reject(error);
 		}
 
-		return Promise.reject(error)
-	}
-)
+		originalRequest._retry = true;
 
-export default request
+		const newAccessToken = await refreshAccessToken();
+		originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+		return request(originalRequest);
+	},
+);
+
+export default request;
