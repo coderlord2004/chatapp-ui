bf8891e663d603b1006e6bad6314bd5bdf4e5f51
minhnbnt <minhnbnt@gmail.com>
2025-06-05 13:08:46 +0700

    dev: re-enable minification

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,20 @@
 import type { NextConfig } from 'next';
+import TerserPlugin from 'terser-webpack-plugin';
 
 const nextConfig: NextConfig = {
 	output: 'export',
 	images: {
 		unoptimized: true, // Tắt tối ưu hóa hình ảnh
 	},
-	webpack: (config) => ({
-		...config,
-		optimization: {
-			minimize: false,
-		},
-	}),
+	webpack: (config) => {
+		config.optimization.minimizer = [
+			new TerserPlugin({
+				exclude: 'src/components/VideoCall.tsx',
+			}),
+		];
+
+		return config;
+	},
 };
 
 export default nextConfig;
